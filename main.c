700fff45128b6ade994f00b73bf1df4ff73f6ff7
Ilyes BOUCHLAGHEM <ibouchla@e3r8p15.42.fr>
2019-06-14 16:02:40 +0200

    add bonus part

--- main.c
+++ main.c
@@ -327,6 +327,10 @@ void	test_puts(void)
 	free((void *)s3);
 }
 
+/*
+**	Part 2 - Fonctions simples mais un peu moins de la libc
+*/
+
 void	test_strlen(void)
 {
 	char	*s1 = "";
@@ -534,6 +538,10 @@ void	test_strdup(void)
 	free((void *)r4);
 }
 
+/*
+**	 Part 3 - Cat
+*/
+
 void	test_cat(void)
 {
 	show_header("cat");
@@ -545,6 +553,10 @@ void	test_cat(void)
 		close(fd);
 	}
 }
+
+/*
+**	 Part Bonus
+*/
 
 void	test_strcpy(void)
 {
@@ -604,35 +616,145 @@ void	test_strcpy(void)
 	show_result(((strcmp(r7, r8)) == 0));
 }
 
+void	test_strncpy(void)
+{
+	char	*s1 = "";
+	char	*s2 = strdup("Hello World\0\nDo you catch this ?..");
+	char	*s3 = strdup("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c");
+	char	*s4 = strdup("In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old. Affronting discretion as do is announcing. Now months esteem oppose nearer enable too six. She numerous unlocked you perceive speedily. Affixed offence spirits or ye of offices between. Real on shot it were four an as. Absolute bachelor rendered six nay you juvenile. Vanity entire an chatty to. Is post each that just leaf no. He connection interested so we an sympathize advantages. To said is it shed want do. Occasional middletons everything so to. Have spot part for his quit may. Enable it is square my an regard. Often merit stuff first oh up hills as he. Servants contempt as although addition dashwood is procured. Interest in yourself an do of numerous feelings cheerful confined. Imagine was you removal raising gravity. Unsatiable understood or expression dissimilar so sufficient. Its party every heard and event gay. Advice he indeed things adieus in number so uneasy. To many four fact in he fail. My hung it quit next do of. It fifteen charmed by private savings it mr. Favourable cultivated alteration entreaties yet met sympathize. Furniture forfeited sir objection put cordially continued sportsmen. And sir dare view but over man. So at within mr to simple assure. Mr disposing continued it offending arranging in we. Extremity as if breakfast agreement. Off now mistress provided out horrible opinions. Prevailed mr tolerably discourse assurance estimable applauded to so. Him everything melancholy uncommonly but solicitude inhabiting projection off. Connection stimulated estimating excellence an to impression. Dispatched entreaties boisterous say why stimulated. Certain forbade picture now prevent carried she get see sitting. Up twenty limits as months. Inhabit so perhaps of in to certain. Sex excuse chatty was seemed warmth. Nay add far few immediate sweetness earnestly dejection. Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or if excited forbade. Here name off yet she long sold easy whom. Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service. Fat new smallness few supposing suspicion two. Course sir people worthy horses add entire suffer. How one dull get busy dare far. At principle perfectly by sweetness do. As mr started arrival subject by believe. Strictly numerous outlived kindness whatever on we no on addition. Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard. Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands. Is be upon sang fond must shew. Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. Merits behind on afraid or warmly.");
+	char	*d1 = calloc(1, 1024);
+	char	*d2 = calloc(1, 1024);
+	char	*d3 = calloc(1, 1024);
+	char	*d4 = calloc(1, 1024);
+	char	*d1_bis = calloc(1, 1024);
+	char	*d2_bis = calloc(1, 1024);
+	char	*d3_bis = calloc(1, 1024);
+	char	*d4_bis = calloc(1, 1024);
+
+	show_header("strcpy");
+
+	/* Start test [01] */
+	printf("\nStart Test [01] :\n");
+	printf("\n---- My function ----\n==> ft_strncpy(%p, \"\", 42))\n", d1);
+	char *r1 = ft_strncpy(d1, s1, 42);
+	ft_hexdump((void *)r1, 16, 16);
+	printf("\n---- Unix function ----\n==>  strncpy(%p, \"\", 42))\n", d1_bis);
+	char *r2 = strncpy(d1_bis, s1, 42);
+	ft_hexdump((void *)r2, 16, 16);
+	show_result(((strcmp(r1, r2)) == 0));
+
+	/* Start test [02] */
+	printf("\nStart Test [02] :\n");
+	printf("\n---- My function ----\n==> ft_strncpy(%p, \"%s\", strlen(s2)))\n", d2, s2);
+	char *r3 = ft_strncpy(d2, s2, strlen(s2));
+	ft_hexdump((void *)r3, strlen(s2), 16);
+	printf("\n---- Unix function ----\n==>  strncpy(%p, \"%s\", strlen(s2)))\n", d2_bis, s2);
+	char *r4 = strncpy(d2_bis, s2, strlen(s2));
+	ft_hexdump((void *)r4, strlen(s2), 16);
+	show_result(((strcmp(r3, r4)) == 0));
+
+	/* Start test [03] */
+	printf("\nStart Test [03] :\n");
+	printf("\n---- My function ----\n==> ft_strncpy(%p, ascii.printable, 10))\n", d3);
+	char *r5 = ft_strncpy(d3, s3, 10);
+	ft_hexdump((void *)r5, strlen(s3), 16);
+	printf("\n---- Unix function ----\n==>  strncpy(%p, ascii.printable, 10))\n", d3_bis);
+	char *r6 = strncpy(d3_bis, s3, 10);
+	ft_hexdump((void *)r6, strlen(s3), 16);
+	show_result(((strcmp(r5, r6)) == 0));
+
+	/* Start test [04] */
+	printf("\nStart Test [04] :\n");
+	printf("\n---- My function ----\n==> ft_strncpy(%p, long text.., 4096))\n", d4);
+	char *r7 = ft_strncpy(d4, s4, 4096);
+	ft_hexdump((void *)r7, strlen(s4), 16);
+	printf("\n---- Unix function ----\n==>  strncpy(%p, long text.., 4096))\n", d4_bis);
+	char *r8 = strncpy(d4_bis, s4, 4096);
+	ft_hexdump((void *)r8, strlen(s4), 16);
+	show_result(((strcmp(r7, r8)) == 0));
+}
+
+void	test_strnlen(void)
+{
+	char	*s1 = "";
+	char	*s2 = strdup("Hello World\0\nDo you catch this ?..");
+	char	*s3 = strdup("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c");
+	char	*s4 = strdup("In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old. Affronting discretion as do is announcing. Now months esteem oppose nearer enable too six. She numerous unlocked you perceive speedily. Affixed offence spirits or ye of offices between. Real on shot it were four an as. Absolute bachelor rendered six nay you juvenile. Vanity entire an chatty to. Is post each that just leaf no. He connection interested so we an sympathize advantages. To said is it shed want do. Occasional middletons everything so to. Have spot part for his quit may. Enable it is square my an regard. Often merit stuff first oh up hills as he. Servants contempt as although addition dashwood is procured. Interest in yourself an do of numerous feelings cheerful confined. Imagine was you removal raising gravity. Unsatiable understood or expression dissimilar so sufficient. Its party every heard and event gay. Advice he indeed things adieus in number so uneasy. To many four fact in he fail. My hung it quit next do of. It fifteen charmed by private savings it mr. Favourable cultivated alteration entreaties yet met sympathize. Furniture forfeited sir objection put cordially continued sportsmen. And sir dare view but over man. So at within mr to simple assure. Mr disposing continued it offending arranging in we. Extremity as if breakfast agreement. Off now mistress provided out horrible opinions. Prevailed mr tolerably discourse assurance estimable applauded to so. Him everything melancholy uncommonly but solicitude inhabiting projection off. Connection stimulated estimating excellence an to impression. Dispatched entreaties boisterous say why stimulated. Certain forbade picture now prevent carried she get see sitting. Up twenty limits as months. Inhabit so perhaps of in to certain. Sex excuse chatty was seemed warmth. Nay add far few immediate sweetness earnestly dejection. Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or if excited forbade. Here name off yet she long sold easy whom. Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service. Fat new smallness few supposing suspicion two. Course sir people worthy horses add entire suffer. How one dull get busy dare far. At principle perfectly by sweetness do. As mr started arrival subject by believe. Strictly numerous outlived kindness whatever on we no on addition. Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard. Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands. Is be upon sang fond must shew. Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. Merits behind on afraid or warmly.");
+
+	show_header("strnlen");
+
+	/* Start test [01] */
+	printf("\nStart Test [01] :\n");
+	printf("\n---- My function ----\n==> ft_strnlen(\"\", 42))\n");
+	size_t r1 = ft_strnlen(s1, 42);
+	printf("\n---- Unix function ----\n==> strnlen(\"\", 42))\n");
+	size_t r2 = strnlen(s1, 42);
+	show_result(r1 == r2);
+
+	/* Start test [02] */
+	printf("\nStart Test [02] :\n");
+	printf("\n---- My function ----\n==> ft_strnlen(\"%s\", 0))\n", s2);
+	size_t r3 = ft_strnlen(s2, 0);
+	printf("\n---- Unix function ----\n==> strnlen(\"%s\", 0))\n", s2);
+	size_t r4 = strnlen(s2, 0);
+	show_result(r3 == r4);
+
+	/* Start test [03] */
+	printf("\nStart Test [03] :\n");
+	printf("\n---- My function ----\n==> ft_strnlen(ascii.printable, strlen(s3)))\n");
+	size_t r5 = ft_strnlen(s3, strlen(s3));
+	printf("\n---- Unix function ----\n==> strnlen(ascii.printable, strlen(s3)))\n");
+	size_t r6 = strnlen(s3, strlen(s3));
+	show_result(r5 == r6);
+
+	/* Start test [04] */
+	printf("\nStart Test [04] :\n");
+	printf("\n---- My function ----\n==> ft_strnlen(long text.., -1))\n");
+	size_t r7 = ft_strnlen(s4, -1);
+	printf("\n---- Unix function ----\n==> strnlen(long text.., -1))\n");
+	size_t r8 = strnlen(s4, -1);
+	show_result(r7 == r8);
+
+	free((void *)s2);
+	free((void *)s3);
+	free((void *)s4);
+}
+
+/*
+**	 Main tests
+*/
+
 int		main(void)
 {
 /*
 **	Part 1 - Fonctions simples de la libc
 */
-
-	test_bzero();
-	test_strcat();
-	test_isalpha();
-	test_isdigit();
-	test_isalnum();
-	test_isascii();
-	test_isprint();
-	test_toupper();
-	test_tolower();
-	test_puts();
-
+	//test_bzero();
+	//test_strcat();
+	//test_isalpha();
+	//test_isdigit();
+	//test_isalnum();
+	//test_isascii();
+	//test_isprint();
+	//test_toupper();
+	//test_tolower();
+	//test_puts();
 /*
 **	Part 2 - Fonctions simples mais un peu moins de la libc
 */
-	test_strlen();
-	test_memset();
-	test_memcpy();
-	test_strdup();
-
+	//test_strlen();
+	//test_memset();
+	//test_memcpy();
+	//test_strdup();
 /*
 **	 Part 3 - Cat
 */
-	test_cat();
-
+	//test_cat();
+/*
+**	Part Bonus
+*/
+	test_strcpy();
+	test_strncpy();
+	test_strnlen();
 	return (0);
 }
